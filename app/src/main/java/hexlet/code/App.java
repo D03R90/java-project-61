909b673fd2ca797e7f17f9c9d1f7f940d3e5cf7d
Vadim Tereschenko <vadim.tereschenko@mail.ru>
2024-03-13 20:57:41 +0300

    Game Calculator

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,34 @@
 package hexlet.code;
 
+import hexlet.code.Games.*;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-//        System.out.println("Welcome to the Brain Games!");
-        Cli.sayHello();
+      StringBuilder allGames = new StringBuilder();
+        System.out.println("Please enter the game number and press Enter.");
+        allGames.append("1 - Greet");
+        allGames.append("\n" + "2 - Even");
+        allGames.append("\n" + "3 - Calc");
+        allGames.append("\n" + "0 - Exit");
+        System.out.println(allGames.toString());
+        System.out.print("Your choice: ");
+        Scanner scStart = new Scanner(System.in);
+        int gameNumber = scStart.nextInt();
+        switch (gameNumber) {
+            case 0:
+                break;
+            case 1:
+                Cli.sayHello();
+                break;
+            case 2:
+                Cli.sayHello();
+                Even.gameEven();
+            case 3:
+                Cli.sayHello();
+                Calculator.gameCalc();
+                break;
+            default:
+                break;
+        }
     }
 }
